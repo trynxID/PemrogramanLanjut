7df68a879fdeb56b74c4355714726a6794bf443a
trynxID <sidiq.firman48@gmail.com>
2022-11-25 15:11:06 +0700

    tampilan kedua

---
+++ Tampilan_awal/Layer_2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tampilan_awal
+{
+    /// <summary>
+    /// Interaction logic for Layer_2.xaml
+    /// </summary>
+    public partial class Layer_2 : Window
+    {
+        public Layer_2()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Tampilan_awal/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tampilan_awal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Name_Click(object sender, RoutedEventArgs e)
+        {
+            Layer_2 objLayer_2 = new Layer_2();
+            this.Visibility = Visibility.Hidden;
+            objLayer_2.Show();
+        }
+    }
+}
